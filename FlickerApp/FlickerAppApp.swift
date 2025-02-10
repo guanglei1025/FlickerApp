ef7af071fefe198f88edfa7ac4977cdd40a7a901
Guanglei Liu <guanglei1025@gmail.com>
2025-02-09 23:33:25 -0600

    Initial Commit

---
+++ FlickerApp/FlickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerAppApp.swift
+//  FlickerApp
+//
+//  Created by Guanglei Liu on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
